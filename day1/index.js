5289ba206cdc04e41b68881d6ffc39573ac3ad13
UWIMPAYE Yvette <nkusiyvette1@gmail.com>
2023-11-22 16:20:37 +0200

    day 2 folder created

---
+++ day1/index.js
@@ -0,0 +1,8 @@
+let name = 'Uwimpaye'; //string
+let age = 20; //number
+age = 20;
+console.log(age);
+let isApproved = true; //boolean
+let person = "yvette";
+console.log(person);
+console.log(Boolean(100));
