f7a6175d5e15d49f85f32de194b97fe47c4bb061
UWIMPAYE Yvette <nkusiyvette1@gmail.com>
2023-11-27 10:22:18 +0200

    day3 added

---
+++ day3/day3.js
@@ -0,0 +1,14 @@
+console.log(Boolean(79))
+console.log(Boolean(-1))
+//equal and not equal operator
+const priceOne = 20;
+const priceTwo = 10;
+console.log(priceOne != priceTwo);//checks if priceone is equal to pricetwo
+console.log(priceOne == priceTwo);
+//greater than and less than operator
+console.log(priceOne > priceTwo);
+console.log(priceTwo > priceOne); 
+let age = 30;
+age = 30;
+console.log(age);
+
