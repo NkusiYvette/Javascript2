eaebc856262562bf9d67b02b6a84090658a11047
UWIMPAYE Yvette <nkusiyvette1@gmail.com>
2023-11-27 15:21:52 +0200

    content added

---
+++ day4/day4.js
@@ -0,0 +1,26 @@
+let brog = 100;
+console.log(brog);
+brog = 300;
+console.log(brog);
+const relative ="cousinne";
+var  height  =  180;
+ {
+         var  weight  =  70;
+         console.log(height);  //  ->  180
+         console.log(weight);  //  ->  70    
+}
+console.log(height);  //  ->  180
+console.log(weight);  //  ->  70
+function testfunction(){
+    console.log("Hello")
+    console.log("Bestie")
+}
+console.log("season:");  //  ->   season:
+console.log("winter");  //  ->  winter
+console.log("rainy");  //  ->  rainy
+console.log("spring:");  //  ->  spring
+console.log("fall");  //  ->  fall
+console.log("summer");  //  ->  summer
+console.log("and  once  more:");  //  ->  and  once  more:
+console.log("summer");  //  ->  summer
+console.log("Winter");  //  ->  Winter
