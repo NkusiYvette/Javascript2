a5c892aa7414e245b4e9514aab7f706d6b6def52
UWIMPAYE Yvette <nkusiyvette1@gmail.com>
2023-12-05 12:32:12 +0200

    New

--- day10/day10.js
+++ day10/day10.js
@@ -1,37 +1,29 @@
-// const INSURANCE_COST = 2.99;
-// let shippingCost = 9.99;
-// let isOrderValid = true;
-// let userAge = 22;
-// let points = 400;
-// let cartValue = 199;
-// let hasPromoCode = false;
-// let hasParentsApproval = false;
-// let addInsurance = true;
+const INSURANCE_COST = 2.99;
+let Cost = 9.99;
+let isOrderValid = true;
+let userAge = 22;
+let points = 400;
+let cartValue = 199;
+let hasPromoCode = false;
+let hasParentsApproval = false;
+let addInsurance = true;
 
-// /** calculate shipping cost*/
-// if ((userAge > 65) || (userAge >= 21 && (hasPromoCode || cartValue > 300 || points > 500))) 
-// {
+/** calculate shipping cost*/
+if ((userAge > 65) || (userAge >= 21 && (hasPromoCode || cartValue > 300 || points > 500))) 
+{
  
-// shippingCost = 0;
-// } else if (userAge < 21 && hasParentsApproval) {
-// shippingCost = shippingCost - 5;
-// } else if (userAge < 21) {
-// isOrderValid = false;
-// }
+Cost = 0;
+} else if (userAge < 21 && hasParentsApproval) {
+Cost =Cost - 5;
+} else if (userAge < 21) {
+isOrderValid = false;
+}
 
-// /** take account of insurance */
-// if (isOrderValid && addInsurance && !hasPromoCode) {
-// shippingCost += INSURANCE_COST;
-// }
+/** take account of insurance */
+if (isOrderValid && addInsurance && !hasPromoCode) {
+Cost += INSURANCE_COST;
+}
 
-/** show message */
-//if (isOrderValid) {
-//console.log(shippingCost);
-
-// else {
-// console.log("Cannot order if under 21");
-// }
-
-let price = 40;
-let shippingCost = price > 50 ?10 : 5;
+let price = 100;
+let shippingCost = price > 50 ? 0 : 5;
 console.log(`price = ${price}, shipping = ${shippingCost}`); // -> price = 100, shipping = 0
