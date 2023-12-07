c7315e2b174c8d7fb8f29c96a4c83ecc5bb7d4cb
UWIMPAYE Yvette <nkusiyvette1@gmail.com>
2023-12-07 16:22:30 +0200

    Switch

--- day10/day10.js
+++ day10/day10.js
@@ -27,3 +27,16 @@ Cost += INSURANCE_COST;
 let price = 100;
 let shippingCost = price > 50 ? 0 : 5;
 console.log(`price = ${price}, shipping = ${shippingCost}`); // -> price = 100, shipping = 0
+
+// Ask the user to input a number
+let userInput = prompt("Enter a number:");
+
+// Convert the user input to a number
+let userNumber = parseFloat(userInput);
+
+// Check if the number is greater than 90 but less than 110
+if (userNumber > 90 && userNumber < 110) {
+  console.log("Bingo!");
+} else {
+  console.log("Miss");
+}
---
+++ work/days.js
@@ -0,0 +1,26 @@
+let days = prompt("choose a number to dispaly a day:1,2,3,4,5,6,7");
+switch (days){
+    case "1":
+    alert("Monday");
+    break;
+    case "2":
+        alert("Tuesday");
+        break;
+        case "3":
+            alert("Wednesday");
+            break;
+            case "4":
+                alert("Thursday");
+                break;
+                case "5":
+                    alert("Friday");
+                    break;
+                    case "6":
+                        alert("Surtaday");
+                        break;
+                        case "7":
+                            alert("Sunday");
+                           default:
+                            alert("Doesn't exist");
+
+}
