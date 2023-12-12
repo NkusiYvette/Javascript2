aefcd0f7e0dd8f99d7aec08c9b8a4f0307eb43e3
UWIMPAYE Yvette <nkusiyvette1@gmail.com>
2023-12-12 11:14:44 +0200

    key

--- day14/day14.js
+++ day14/day14.js
@@ -1,7 +1,25 @@
-let fEC = {
-    name : "bebe",
-    age : 18,
-    sex : "female",
-    graduate : false
+// let fEC = {
+//     name : "bebe",
+//     age : 18,
+//     sex : "female",
+//     graduate : false
+//  }
+//  console.log(fEC.name,fEC.age);
+
+ //application age
+
+//  let age = prompt("Enter age: age");
+ function checkAge(age){
+    if (age >= 18){
+        console.log("Enjoy our site");
+    }
+    else{
+        console.log("You're not allowed");
+    }
  }
- console.log(fEC.name,fEC.age);
+ let age = 18;
+ checkAge(age);
+
+for(let i =0; i<= 10; i++){
+    console.log(i);
+}
