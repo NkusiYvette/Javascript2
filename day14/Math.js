0a511aede7a2f4ddf253d3da2de6562351955af1
UWIMPAYE Yvette <nkusiyvette1@gmail.com>
2023-12-13 09:51:45 +0200

    math

---
+++ day14/Math.js
@@ -0,0 +1,17 @@
+//sqrt
+let num = 9;
+console.log(Math.sqrt(num));
+//absolute
+console.log(Math.abs(-144));
+//power
+console.log(Math.pow(100,2));
+//round
+console.log(Math.round(6.7));
+//ceil
+//floor
+//exponent
+//log,sin,cos,tan
+//PI
+//Random
+//max,min
+//trunc
--- day14/timer.js
+++ day14/timer.js
@@ -3,19 +3,22 @@
 // }
 // setTimeout(BossBabes,100); 
 //interval
-let count = 0;
-function increment(){
-    count++;
-    console.log(count);
-}
-setInterval(increment, 100);
+// let count = 0;
+// function increment(){
+//     count++;
+//     console.log(count);
+// }
+// setInterval(increment, 100);
 
-//cleartimeout
+// //cleartimeout
 
-function greet() {
-    console.log("Hello world");
-  }
-  const timeoutID = setTimeout(greet, 2000);
-  clearTimeout(
-    timeoutID
-  ); 
+// function greet() {
+//     console.log("Hello world");
+//   }
+//   const timeoutID = setTimeout(greet, 2000);
+//   clearTimeout(
+//     timeoutID
+//   ); 
+let name = "bebe";
+let change = name.split('').reverse().join('');
+ console.log(change);
